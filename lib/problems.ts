e4be2d2082337252f93e75a84b600267a80a3e14
Ankush Kumar <imankush1010@gmail.com>
2025-09-20 03:26:02 +0530

    Add checkOrder property to test cases

--- lib/problems.ts
+++ lib/problems.ts
@@ -1,6 +1,7 @@
 export interface TestCase {
   input: string;
   expectedOutput: string;
+  checkOrder:boolean;
 }
 
 export interface LanguageConfig {
@@ -28,11 +29,11 @@ export const PROBLEMS_DB: Problem[] = [
     description:
       "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
     testCases: [
-      { input: "[2,7,11,15],9", expectedOutput: "[0,1]" },
-      { input: "[3,2,4],6", expectedOutput: "[1,2]" },
-      { input: "[3,3],6", expectedOutput: "[0,1]" },
-      { input: "[1,2,3,4,5],9", expectedOutput: "[3,4]" },
-      { input: "[-1,-2,-3,-4,-5],-8", expectedOutput: "[2,4]" },
+      { input: "[2,7,11,15],9", expectedOutput: "[0,1]",checkOrder: false },
+      { input: "[3,2,4],6", expectedOutput: "[1,2]" checkOrder: false },
+      { input: "[3,3],6", expectedOutput: "[0,1]", checkOrder: false },
+      { input: "[1,2,3,4,5],9", expectedOutput: "[3,4]", checkOrder: false },
+      { input: "[-1,-2,-3,-4,-5],-8", expectedOutput: "[2,4]", checkOrder: false },
     ],
     languages: {
       cpp: {
