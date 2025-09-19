82b5f3b6641a77c302c95c7d74280c3a3f55dca8
Ankush Kumar <imankush1010@gmail.com>
2025-09-20 03:28:34 +0530

    Fix syntax error in test case for two sum problem

--- lib/problems.ts
+++ lib/problems.ts
@@ -30,7 +30,7 @@ export const PROBLEMS_DB: Problem[] = [
       "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
     testCases: [
       { input: "[2,7,11,15],9", expectedOutput: "[0,1]",checkOrder: false },
-      { input: "[3,2,4],6", expectedOutput: "[1,2]" checkOrder: false },
+      { input: "[3,2,4],6", expectedOutput: "[1,2]", checkOrder: false },
       { input: "[3,3],6", expectedOutput: "[0,1]", checkOrder: false },
       { input: "[1,2,3,4,5],9", expectedOutput: "[3,4]", checkOrder: false },
       { input: "[-1,-2,-3,-4,-5],-8", expectedOutput: "[2,4]", checkOrder: false },
